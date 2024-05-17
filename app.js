de2c98bbbf108b0a46ab263e3c2b3e7a86ec2b95
Juan Manuel Vargas <juanmavargasr@gmail.com>
2024-05-17 13:44:42 -0500

    updating app.js to fix auth problems

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const { connectDB } = require("./src/db");
 const app = express();
 require("dotenv").config();
 const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
 const javier = {
@@ -12,12 +13,14 @@ const javier = {
 
 const port = process.env.SECRET_PORT;
 const userRoutes = require("./src/routes/userRoutes");
+const authRoutes = require("./src/routes/authRoutes");
 
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(cors(javier));
 
 app.use("/users", userRoutes);
+app.use("/logIn", authRoutes);
 
 connectDB();
 app.listen(port, () => {
